c4d9d42357eae9f78be1fbdcf1b75aa553e627da
ayushChauhan <ayush.chauhan@flick2know.com>
2026-06-30 18:36:48 +0530

    Add project files.

---
+++ Models/UserConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace FormulaOne.ChatService.Models
+{
+    public class UserConnection
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string UserConnectionId { get; set; } = string.Empty;
+    }
+}
